04c854e7875553e0f8dbff259a57750debdd68a8
lawrence18365 <lawrencebrennan@gmail.com>
2024-07-04 10:01:41 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pricingButton = document.querySelector('button');
+    pricingButton.addEventListener('click', () => {
+        alert('Pricing information coming soon!');
+    });
+});
